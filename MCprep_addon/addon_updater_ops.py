8ae542922cf35c8d2124279782560fa9df1b3cdf
TheDuckCow <theduckcow@live.com>
2017-05-23 06:34:28 -0400

    one more quick fix to name of addon in updater section

--- MCprep_addon/addon_updater_ops.py
+++ MCprep_addon/addon_updater_ops.py
@@ -41,7 +41,7 @@ except Exception as e:
 # Must declare this before classes are loaded
 # otherwise the bl_idname's will not match and have errors.
 # Must be all lowercase and no spaces
-updater.addon = "addon_updater_demo"
+updater.addon = "mcprep"
 
 
 # -----------------------------------------------------------------------------
