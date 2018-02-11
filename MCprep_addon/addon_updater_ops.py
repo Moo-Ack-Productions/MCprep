2e40008356e4f5f0ae371eeee111d75da954d49c
TheDuckCow <theduckcow@live.com>
2018-02-11 08:50:15 -0500

    putting back in correct updater addon setting

--- MCprep_addon/addon_updater_ops.py
+++ MCprep_addon/addon_updater_ops.py
@@ -42,7 +42,7 @@ except Exception as e:
 # Must declare this before classes are loaded
 # otherwise the bl_idname's will not match and have errors.
 # Must be all lowercase and no spaces
-updater.addon = "addon_updater_demo"
+updater.addon = "mcprep"
 
 
 # -----------------------------------------------------------------------------
