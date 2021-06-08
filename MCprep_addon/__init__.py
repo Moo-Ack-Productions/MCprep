eca41418c3ce0ef9a1bab8c2fff95e8251381121
Patrick W. Crawford <theduckcow@live.com>
2021-06-08 14:24:49 -0400

    Updated version and verbose status for MCprep v3.2.5 release

--- MCprep_addon/__init__.py
+++ MCprep_addon/__init__.py
@@ -37,7 +37,7 @@ Disclaimer: This is not an official Google product
 bl_info = {
 	"name": "MCprep",
 	"category": "Object",
-	"version": (3, 2, 4, 3),
+	"version": (3, 2, 5),
 	"blender": (2, 80, 0),
 	"location": "3D window toolshelf > MCprep tab",
 	"description": "Minecraft workflow addon for rendering and animation",
