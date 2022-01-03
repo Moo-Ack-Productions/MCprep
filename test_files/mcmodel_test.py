b8c4c6208692458fc2716bfa8cbae403d6bbc7fb
Patrick W. Crawford <theduckcow@live.com>
2022-01-03 01:03:26 -0800

    Minor edge handle fixes based on early model reports, plus model test script
    Test script can be used manually to see which if any models cause errors on run. Right now, out of 1.6K, 10 cause errors when running. These are special cases which may later be removed from the default listing of models (as they are more template like it seems)

---
+++ test_files/mcmodel_test.py
@@ -0,0 +1,62 @@
+"""Test MC Model spawning by iterating through whole list.
+
+Will be slow, spawning each one, and listing which ones have errors.
+"""
+
+import bpy
+
+MAX_CHECK = 2000
+
+
+def check_models(context):
+	bpy.ops.mcprep.reload_models()
+
+	scn_props = context.scene.mcprep_props
+	scn_props.model_list_index = 0
+
+	exceptions = []
+	successful = []
+
+	count = len(scn_props.model_list)
+
+	print("Total to process: ", count)
+
+	for index in range(len(scn_props.model_list)):
+		# scn_props.model_list_index = index
+		model = scn_props.model_list[index]
+
+		try:
+			bpy.ops.mcprep.spawn_model(filepath=model.filepath, skipUsage=True)
+
+			new_obj = context.object
+			if len(new_obj.data.vertices) < 4:
+				exceptions.append([model.name, "Not enough geo generated"])
+				print("#{} {} failed".format(
+					index, model.name))
+			else:
+				successful.append(model.name)
+		except Exception as err:
+			exceptions.append([model.name, str(err)])
+			print(f"{model.name} failed")
+		finally:
+			print("#{}/{}".format(index, count))
+
+		bpy.ops.object.delete(use_global=True)
+
+		if MAX_CHECK <= index:
+			break
+
+	print("Succeeded: {}, failed: {}".format(
+		len(successful),
+		len(exceptions)))
+
+	if not exceptions:
+		print("No failures!")
+	else:
+		print("Those that failed:")
+		for itm in exceptions:
+			print("{}: {}".format(itm[0], itm[1]))
+
+
+check_models(bpy.context)
+print("Finished check")
