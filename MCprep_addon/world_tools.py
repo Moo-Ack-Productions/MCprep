42a3dca1d97b0cf6f88b0656a34bf4d15a3bfc91
tauchner <7229@htl.rennweg.at>
2022-07-03 17:01:30 +0200

    Added panorama render UI

--- MCprep_addon/world_tools.py
+++ MCprep_addon/world_tools.py
@@ -973,6 +973,24 @@ def world_time_update(self, context):
 	return
 
 
+class MCPREP_OT_render_panorama(bpy.types.Operator):
+	"""Render the Panorama images for a texture Pack"""
+	bl_idname = "mcprep.render_panorama"
+	bl_label = "Render Panorama"
+	bl_description = "Render Panorama for texture Pack"
+	bl_options = {'REGISTER', 'UNDO'}
+
+	def invoke(self, context, event):
+		return context.window_manager.invoke_props_dialog(self, width=400 * util.ui_scale())
+
+	def draw(self, context):
+		self.layout.prop(self, "panorama_resolution")
+		self.layout.prop(self, "save_path")
+
+	def execute(self, context):
+		return {'FINISHED'}
+
+
 # -----------------------------------------------------------------------------
 # Above for UI
 # Below for register
