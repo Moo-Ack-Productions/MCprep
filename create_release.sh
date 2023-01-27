c9243275f5d79e40e7d9227f21e2c6483b34fcb9
mahid <mahidsheikh@pm.me>
2023-01-27 17:27:25 -0600

    Updated create_release

--- create_release.sh
+++ create_release.sh
@@ -1,5 +1,5 @@
 wget "https://github.com/TheDuckCow/MCprep/releases/download/3.4.1/MCprep_addon_v3.4.1.zip"
 mkdir MCprep_stable_release
-unzip ./MCprep_addon_v3.4.1.zip -d MCprep_stable_release
+unzip ./MCprep_addon_v*.zip -d MCprep_stable_release
 mv ./MCprep_stable_release/MCprep_addon/MCprep_resources/** ./MCprep_addon/MCprep_resources
-rm -rf ./MCprep_stable_release MCprep_addonv3.4.1.zip
+rm -rf ./MCprep_stable_release MCprep_addonv_*.zip
