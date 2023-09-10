b690f93c3649d1dd2aafde3e9e64b062e2c33a8b
StandingPad Animations <mahidsheikh@pm.me>
2023-09-10 11:57:06 -0500

    Added files related to creating release candidates
    This commit adds the necesary files to create an MCprep release
    candidate:
    - download_resources.sh: automatically downloads and moves assets
      from the latest stable release of MCprep
    - rc-bl_info.patch: patches bl_info for release candidates

---
+++ rc-files/download_resources.sh
@@ -0,0 +1,11 @@
+# Get the latest stable release and unzip
+wget "https://github.com/TheDuckCow/MCprep/releases/download/3.4.3/MCprep_addon_v3.4.3.zip"
+mkdir MCprep_stable_release
+unzip ./MCprep_addon_v*.zip -d MCprep_stable_release
+
+# Remove the files in resources
+rm -rf ./MCprep_addon/MCprep_resources/**
+# Move the special files
+mv -f ./MCprep_stable_release/MCprep_addon/MCprep_resources/** ./MCprep_addon/MCprep_resources 
+# Remove the stable release
+rm -rf ./MCprep_stable_release MCprep_addon_v*.zip
