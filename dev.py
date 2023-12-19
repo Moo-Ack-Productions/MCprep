df17952e6b96967355b9243b704a3bf3c19f32ec
StandingPad Animations <mahidsheikh@pm.me>
2023-12-19 09:40:57 -0600

    Upgrade to BpyBuild 0.3.0

---
+++ dev.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    with open("mcprep_dev.txt", "w") as f:
+        f.write("This file is used to enable debugging mode in MCprep, you can ignore this")
+
+if __name__ == "__main__":
+    main()
