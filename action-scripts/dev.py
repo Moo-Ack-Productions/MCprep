359e21c8540c55deb0f10685800ae33fb026dbe0
StandingPad Animations <mahidsheikh@pm.me>
2023-12-21 09:59:52 -0600

    Added print statement to dev.py

--- action-scripts/dev.py
+++ action-scripts/dev.py
@@ -1,4 +1,5 @@
 def main() -> None:
+    print("Dev build, adding mcprep_dev.txt...")
     with open("mcprep_dev.txt", "w") as f:
         f.write("This file is used to enable debugging mode in MCprep, you can ignore this")
 
