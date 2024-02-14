b999e44d6df75b0a0d9c763c1cec38ccbbabad32
StandingPad Animations <mahidsheikh@pm.me>
2024-02-14 08:43:02 -0600

    build: Added separate translate action

--- action-scripts/default.py
+++ action-scripts/default.py
@@ -1,5 +1,3 @@
-from default_scripts import ignore_filters, build_trans_dict, compile_po_to_mo
+from default_scripts import ignore_filters
 
 ignore_filters.main()
-build_trans_dict.main()
-compile_po_to_mo.main()
---
+++ action-scripts/translate.py
@@ -0,0 +1,4 @@
+from translate_scripts import build_trans_dict, compile_po_to_mo
+
+compile_po_to_mo.main()
+build_trans_dict.main()
