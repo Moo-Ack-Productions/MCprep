8d4efc06c33ba26b325b6256b93b38d6425e3667
StandingPad Animations <mahidsheikh@pm.me>
2024-03-30 15:31:29 -0500

    Added email to copyright in commonmcobj_parser.py

--- MCprep_addon/commonmcobj_parser.py
+++ MCprep_addon/commonmcobj_parser.py
@@ -1,6 +1,6 @@
 # BSD 3-Clause License
 # 
-# Copyright (c) 2024, Mahid Sheikh
+# Copyright (c) 2024, Mahid Sheikh <mahid@standingpad.org>
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
