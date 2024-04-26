0bd7cc6eb2a09dab99c290fd96cd8ca22ae213f5
StandingPad Animations <mahidsheikh@pm.me>
2024-04-26 14:32:46 -0500

    build: Moved build-pot to translate action

--- action-scripts/translate.py
+++ action-scripts/translate.py
@@ -1,4 +1,8 @@
-from translate_scripts import build_trans_dict, compile_po_to_mo
+from translate_scripts import build_pot, build_trans_dict, compile_po_to_mo
+from bpy_addon_build.api import BabContext
+
+def pre_build(ctx: BabContext) -> None:
+    build_pot.pre_build(ctx)
 
 def main():
     compile_po_to_mo.main()
