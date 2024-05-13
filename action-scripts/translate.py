e54a4d1d2d446b647b18975d83da5056121d37b2
Mahid Sheikh <mahid@standingpad.org>
2024-05-13 08:52:56 -0500

    build: Fixed issue with BpyError return values
    Signed-off-by: Mahid Sheikh <mahid@standingpad.org>

--- action-scripts/translate.py
+++ action-scripts/translate.py
@@ -1,15 +1,24 @@
+from typing import Optional
 from translate_scripts import build_pot, build_trans_dict, compile_po_to_mo
-from bpy_addon_build.api import BabContext
+from bpy_addon_build.api import BabContext, BpyError
 import sys
 
-def pre_build(ctx: BabContext) -> None:
+def pre_build(ctx: BabContext) -> Optional[BpyError]:
     sys.stdout.reconfigure(encoding='utf-8')
-    build_pot.pre_build(ctx)
+    return build_pot.pre_build(ctx)
 
 def main():
     sys.stdout.reconfigure(encoding='utf-8')
-    compile_po_to_mo.main()
-    build_trans_dict.main()
+    
+    # Go through each module, and 
+    # run its main function, while checking 
+    # for a return value
+    modules = [compile_po_to_mo, build_trans_dict]
+    for mod in modules:
+        res = mod.main()
+        if res:
+            continue 
+        return res
 
 if __name__ == "__main__":
     main()
