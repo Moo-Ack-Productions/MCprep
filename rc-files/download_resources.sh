d9302bfdc51193549fea897699af3d2713e735f9
Mahid Sheikh <mahid@standingpad.org>
2024-06-17 00:46:36 -0500

    rc-files: Update resources link
    Signed-off-by: Mahid Sheikh <mahid@standingpad.org>

--- rc-files/download_resources.sh
+++ rc-files/download_resources.sh
@@ -1,11 +1,11 @@
 # Get the latest stable release and unzip
-wget "https://github.com/TheDuckCow/MCprep/releases/download/3.4.3/MCprep_addon_v3.4.3.zip"
+wget "https://github.com/Moo-Ack-Productions/MCprep/releases/download/3.5.3/MCprep_addon_3.5.3.zip"
 mkdir MCprep_stable_release
-unzip ./MCprep_addon_v*.zip -d MCprep_stable_release
+unzip ./MCprep_addon_*.zip -d MCprep_stable_release
 
 # Remove the files in resources
 rm -rf ./MCprep_addon/MCprep_resources/**
 # Move the special files
 mv -f ./MCprep_stable_release/MCprep_addon/MCprep_resources/** ./MCprep_addon/MCprep_resources 
 # Remove the stable release
-rm -rf ./MCprep_stable_release MCprep_addon_v*.zip
+rm -rf ./MCprep_stable_release MCprep_addon_*.zip
