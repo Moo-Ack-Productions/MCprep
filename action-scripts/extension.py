d2ce049cc69fb872d346bbc73eead38b0038504f
Mahid Sheikh <mahid@standingpad.org>
2024-08-20 08:45:01 -0500

    build: Rename blender_manifest.toml to avoid bugs with 4.2 and bl_info
    Signed-off-by: Mahid Sheikh <mahid@standingpad.org>

--- action-scripts/extension.py
+++ action-scripts/extension.py
@@ -1,3 +1,4 @@
+import os
 from bpy_addon_build.api import BabContext
 import subprocess
 from pathlib import Path
@@ -7,6 +8,10 @@ EXTENSION_BL_INFO_PATCH = Path(__file__).parent.joinpath("build-patches/extensio
 def pre_build(ctx: BabContext) -> None:
     print("Applying Extension Patches")
     _ = subprocess.run(["git", "apply", str(EXTENSION_BL_INFO_PATCH)], cwd=ctx.current_path.parent)
+
+def main(ctx: BabContext) -> None:
+    print("Setting up the Blender Manifest")
+    os.rename("_blender_manifest.toml", "blender_manifest.toml")
 
 def clean_up(ctx: BabContext) -> None:
     print("Cleaning up patches")
