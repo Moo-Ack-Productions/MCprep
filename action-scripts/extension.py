2cfbd2db8c1962e4940a970cd47b4b9120d59f54
Mahid Sheikh <mahid@standingpad.org>
2024-10-10 16:04:02 -0500

    build: Updated extension action to use lib_bpybuild_ext
    Signed-off-by: Mahid Sheikh <mahid@standingpad.org>

--- action-scripts/extension.py
+++ action-scripts/extension.py
@@ -1,5 +1,6 @@
 import os
 from bpy_addon_build.api import BabContext
+from lib_bpybuild_ext import BLENDER_MANIFEST, compat, get_manifest_data, verify
 import subprocess
 from pathlib import Path
 
@@ -11,7 +12,12 @@ def pre_build(ctx: BabContext) -> None:
 
 def main(ctx: BabContext) -> None:
     print("Setting up the Blender Manifest")
-    os.rename("_blender_manifest.toml", "blender_manifest.toml")
+    os.rename(Path(ctx.current_path, "_blender_manifest.toml"), Path(ctx.current_path,"blender_manifest.toml"))
+    print("Performing validation of extension")
+    manifest_path = Path(ctx.current_path, BLENDER_MANIFEST)
+    manifest_data = get_manifest_data(manifest_path)
+    verify.verify_manifest(manifest_data, manifest_path)
+    compat.check_for_compat_issues(ctx.current_path, ctx.builtin_config.addon_folder)
 
 def clean_up(ctx: BabContext) -> None:
     print("Cleaning up patches")
