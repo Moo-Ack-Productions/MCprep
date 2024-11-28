a4c0559bdc316cd91a10ef617c2a451749f6cff8
Mahid Sheikh <mahid@standingpad.org>
2024-11-27 20:49:29 -0600

    feat: Add refinements to Vivy UI
    Signed-off-by: Mahid Sheikh <mahid@standingpad.org>

--- MCprep_addon/vivy_editor.py
+++ MCprep_addon/vivy_editor.py
@@ -4,9 +4,16 @@ from .conf import env
 from .util import is_vivy_enabled
 from .materials import vivy_materials
 
+# Placeholder for None since bpy doesn't allow
+# setting properties to None
+#
+# If a user-supplied value matches this, I don't
+# know what to say
+MCPREP_VIVY_NONE = "@MCPREP_VIVY_NONE"
+
 class ListVivyMaterials(bpy.types.PropertyGroup):
     def items_materials(self, context):
-        items = []
+        items = [(MCPREP_VIVY_NONE, "", "")]
         for mat in bpy.data.materials:
             items.append((mat.name, mat.name, ""))
         return items
@@ -27,6 +34,14 @@ class ListVivyMaterials(bpy.types.PropertyGroup):
     specular: bpy.props.StringProperty(name="Specular Pass", description="Name of node that holds specular pass", default="SPECULAR")
     normal: bpy.props.StringProperty(name="Normal Pass", description="Name of node that holds normal pass", default="NORMAL")
 
+    emissive: bpy.props.EnumProperty(name="Emit refinement", items=items_materials, description="Material for emmisive materials")
+    reflective: bpy.props.EnumProperty(name="Reflective refinement", items=items_materials, description="Material for reflective materials")
+    metallic: bpy.props.EnumProperty(name="Metallic refinement", items=items_materials, description="Material for metallic materials")
+    glass: bpy.props.EnumProperty(name="Glass refinement", items=items_materials, description="Material for glass materials")
+    fallback_s: bpy.props.EnumProperty(name="Fallback specular refinement", items=items_materials, description="Material for missing specular materials")
+    fallback_n: bpy.props.EnumProperty(name="Fallback normal refinement", items=items_materials, description="Material for missing normal materials")
+    fallback: bpy.props.EnumProperty(name="Fallback refinement", items=items_materials, description="Material for missing specular and normal materials")
+
 class VivyEditorProps(bpy.types.PropertyGroup):
     vivy_materials: bpy.props.CollectionProperty(type=ListVivyMaterials)
     vivy_materials_index: bpy.props.IntProperty(default=0)
@@ -46,6 +61,17 @@ def reload_vivy_materials(context):
             vmat.diffuse = passes_data["diffuse"] if "diffuse" in passes_data else ""
             vmat.specular = passes_data["specular"] if "specular" in passes_data else ""
             vmat.normal = passes_data["normal"] if "normal" in passes_data else ""
+
+            # refinements
+            if "refinements" in material_data[mat]:
+                refinements_data = material_data[mat]["refinements"]
+                vmat.emissive = refinements_data["emissive"] if "emissive" in refinements_data else MCPREP_VIVY_NONE
+                vmat.reflective = refinements_data["reflective"] if "reflective" in refinements_data else MCPREP_VIVY_NONE
+                vmat.metallic = refinements_data["metallic"] if "metallic" in refinements_data else MCPREP_VIVY_NONE
+                vmat.glass = refinements_data["glass"] if "glass" in refinements_data else MCPREP_VIVY_NONE
+                vmat.fallback_s = refinements_data["fallback_s"] if "fallback_s" in refinements_data else MCPREP_VIVY_NONE
+                vmat.fallback_n = refinements_data["fallback_n"] if "fallback_n" in refinements_data else MCPREP_VIVY_NONE
+                vmat.fallback = refinements_data["fallback"] if "fallback" in refinements_data else MCPREP_VIVY_NONE
 
 class VIVY_UL_materials(bpy.types.UIList):
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
@@ -70,9 +96,11 @@ class VIVY_OT_apply_changes(bpy.types.Operator):
     bl_idname = "vivy.apply_changes"
     bl_label = "Apply Changes to Current Material"
     bl_options = {'REGISTER', 'UNDO'}
-
+    
+    # TODO: Find a more elegant way to handle
+    # all of this.
     def execute(self, context):
-        env.reload_vivy_json()
+        env.reload_vivy_json(context.scene.vivy_file_path)
         if env.vivy_material_json is not None and "materials" in env.vivy_material_json:
             vprop = context.scene.vivy_editor_props
             mat = vprop.vivy_materials[vprop.vivy_materials_index]
@@ -95,9 +123,20 @@ class VIVY_OT_apply_changes(bpy.types.Operator):
                 
                 # passes
                 orig_passes = original_data["passes"]
-                orig_passes["diffuse"] = name.diffuse
-                orig_passes["specular"] = name.specular
-                orig_passes["normal"] = name.normal
+                orig_passes["diffuse"] = mat.diffuse if mat.diffuse != "" else MCPREP_VIVY_NONE
+                orig_passes["specular"] = mat.specular if mat.specular != "" else MCPREP_VIVY_NONE
+                orig_passes["normal"] = mat.normal if mat.normal != "" else MCPREP_VIVY_NONE
+                
+                # refinements
+                orig_refinements = original_data["refinements"]
+                orig_refinements["emissive"] = mat.emissive
+                orig_refinements["reflective"] = mat.reflective
+                orig_refinements["metallic"] = mat.metallic
+                orig_refinements["glass"] = mat.glass
+                orig_refinements["fallback_s"] = mat.fallback_s
+                orig_refinements["fallback_n"] = mat.fallback_n
+                orig_refinements["fallback"] = mat.fallback
+
                 data["materials"][mat.name] = original_data
                 
                 # Update mappings as well for
@@ -113,11 +152,39 @@ class VIVY_OT_apply_changes(bpy.types.Operator):
                     "desc": mat.desc,
                     "base_material": mat.base_material,
                     "passes" : {
-                        "diffuse": mat.diffuse,
-                        "specular": mat.specular,
-                        "normal": mat.normal
+                        "diffuse": mat.diffuse if mat.diffuse != "" else MCPREP_VIVY_NONE,
+                        "specular": mat.specular if mat.specular != "" else MCPREP_VIVY_NONE,
+                        "normal": mat.normal if mat.normal != "" else MCPREP_VIVY_NONE
+                    },
+                    "refinements" : {
+                        "emissive": mat.emissive,
+                        "reflective": mat.reflective,
+                        "metallic": mat.metallic,
+                        "glass": mat.glass,
+                        "fallback_s": mat.fallback_s,
+                        "fallback_n": mat.fallback_n,
+                        "fallback": mat.fallback
                     }
                 }
+            
+            for key in data["materials"]:
+                passes = data["materials"][key]["passes"] if "passes" in data["materials"][key] else {}
+                refinements = data["materials"][key]["refinements"] if "refinements" in data["materials"][key] else {}
+
+                c_passes = passes.copy()
+                c_refinements = refinements.copy()
+                
+                for pkey in passes:
+                    if passes[pkey] == MCPREP_VIVY_NONE:
+                        c_passes.pop(pkey)
+                for rkey in refinements:
+                    if refinements[rkey] == MCPREP_VIVY_NONE:
+                        c_refinements.pop(rkey)
+                
+                if len(c_passes):
+                    data["materials"][key]["passes"] = c_passes
+                if len(c_refinements):
+                    data["materials"][key]["refinements"] = c_refinements
 
             json_path = vivy_materials.get_vivy_json()
             with open(json_path, 'w') as f:
@@ -162,6 +229,21 @@ class VIVY_PT_editor(bpy.types.Panel):
             row.prop(mat, "specular")
             row = box.row()
             row.prop(mat, "normal")
+            row = box.row()
+            row.prop(mat, "emissive")
+            row = box.row()
+            row.prop(mat, "reflective")
+            row = box.row()
+            row.prop(mat, "metallic")
+            row = box.row()
+            row.prop(mat, "glass")
+            row = box.row()
+            row.prop(mat, "fallback_s")
+            row = box.row()
+            row.prop(mat, "fallback_n")
+            row = box.row()
+            row.prop(mat, "fallback")
+
             layout.operator("vivy.apply_changes")
         else:
             layout.operator("vivy.reload_editor")   
