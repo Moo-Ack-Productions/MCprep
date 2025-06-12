a62abe11ed218dbeb45eca87725de9897ece9589
Mahid Sheikh <mahid@standingpad.org>
2025-06-12 18:08:56 -0500

    build(rc): Make download_resources.sh restore Languages and JSON
    To avoid issues when building MCprep (and for convinience reasons), the
    download_resources.sh script has been updated to restore the `Languages`
    and `mcprep_data_update.json` files after introducing the other assets.
    
    Signed-off-by: Mahid Sheikh <mahid@standingpad.org>

--- rc-files/download_resources.sh
+++ rc-files/download_resources.sh
@@ -12,3 +12,6 @@ rm -rf ./MCprep_addon/MCprep_resources/**
 mv -f ./MCprep_stable_release/MCprep_addon/MCprep_resources/** ./MCprep_addon/MCprep_resources 
 # Remove the stable release
 rm -rf ./MCprep_stable_release MCprep_addon_*.zip
+
+git restore MCprep_addon/MCprep_resources/Languages
+git restore MCprep_addon/MCprep_resources/mcprep_data_update.json
