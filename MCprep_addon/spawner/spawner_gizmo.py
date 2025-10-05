a18d7875ef80481534ba426a99ecb440fd3870d9
Mahid Sheikh <mahid@standingpad.org>
2025-10-04 22:11:21 -0500

    style: Format spawner_gizmo.py as per PEP8
    Signed-off-by: Mahid Sheikh <mahid@standingpad.org>

--- MCprep_addon/spawner/spawner_gizmo.py
+++ MCprep_addon/spawner/spawner_gizmo.py
@@ -11,11 +11,13 @@ from mathutils.geometry import intersect_line_plane
 ZERO_VECTOR = Vector((0, 0, 0))
 UP_VECTOR = Vector((0, 0, 1))
 
+
 @dataclass
 class HitVector:
     location: Vector
     normal: Vector
     rotation: Quaternion
+
 
 def draw_fading_grid(shader_info, size, subdivisions, rings, base_color):
     """Creates a grid with a fading circle gradient"""
@@ -57,30 +59,37 @@ def draw_fading_grid(shader_info, size, subdivisions, rings, base_color):
         if not coords:
             continue
 
-        batch = batch_for_shader(shader_info, 'LINES', {"pos": coords})
+        batch = batch_for_shader(shader_info, "LINES", {"pos": coords})
         shader_info.bind()
         shader_info.uniform_float("color", color)
         batch.draw(shader_info)
 
+
 def draw_callback(hit_vector: HitVector) -> None:
     """Callback function to call when drawing the gizmo"""
-    shader_info = gpu.shader.from_builtin('UNIFORM_COLOR')
+    shader_info = gpu.shader.from_builtin("UNIFORM_COLOR")
 
     original_blend = gpu.state.blend_get()
     original_depth_test = gpu.state.depth_test_get()
-    gpu.state.blend_set('ALPHA')
-    gpu.state.depth_test_set('NONE')
+    gpu.state.blend_set("ALPHA")
+    gpu.state.depth_test_set("NONE")
 
-    transform_matrix = Matrix.Translation(hit_vector.location) @ hit_vector.rotation.to_matrix().to_4x4()
+    transform_matrix = (
+        Matrix.Translation(hit_vector.location)
+        @ hit_vector.rotation.to_matrix().to_4x4()
+    )
 
     gpu.matrix.push()
     gpu.matrix.multiply_matrix(transform_matrix)
 
-    draw_fading_grid(shader_info, size=2.0, subdivisions=20, rings=10, base_color=(0.7, 0.7, 0.7))
+    draw_fading_grid(
+        shader_info, size=2.0, subdivisions=20, rings=10, base_color=(0.7, 0.7, 0.7)
+    )
 
     gpu.matrix.pop()
     gpu.state.blend_set(original_blend)
     gpu.state.depth_test_set(original_depth_test)
+
 
 def update_raycast(context, event) -> Optional[HitVector]:
     """Given the context of the scene, update the location, normal, and rotation of the ray"""
@@ -89,12 +98,16 @@ def update_raycast(context, event) -> Optional[HitVector]:
     ray_origin = view3d_utils.region_2d_to_origin_3d(region, region_3d, mouse_pos)
     ray_direction = view3d_utils.region_2d_to_vector_3d(region, region_3d, mouse_pos)
     depsgraph = context.evaluated_depsgraph_get()
-    result, location, normal, _, _, _ = context.scene.ray_cast(depsgraph, ray_origin, ray_direction)
+    result, location, normal, _, _, _ = context.scene.ray_cast(
+        depsgraph, ray_origin, ray_direction
+    )
 
     if result:
         return HitVector(location, normal, UP_VECTOR.rotation_difference(normal))
     else:
-        intersection = intersect_line_plane(ray_origin, ray_origin + ray_direction, ZERO_VECTOR, UP_VECTOR)
+        intersection = intersect_line_plane(
+            ray_origin, ray_origin + ray_direction, ZERO_VECTOR, UP_VECTOR
+        )
         if intersection:
             return HitVector(intersection, UP_VECTOR, Quaternion())
     # If there is no hit, then don't return a HitVector
