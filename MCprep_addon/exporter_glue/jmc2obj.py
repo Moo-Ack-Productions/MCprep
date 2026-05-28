f18749fc7a55585f7dcca8ceb69e4131bc1df238
Maryam Sheikh <mahid@standingpad.org>
2026-05-27 22:44:24 -0500

    fix: Handle centered exports in jmc2OBJ
    Signed-off-by: Maryam Sheikh <mahid@standingpad.org>

--- MCprep_addon/exporter_glue/jmc2obj.py
+++ MCprep_addon/exporter_glue/jmc2obj.py
@@ -12,6 +12,7 @@ class Jmc2OBJExportBuilder(ExportBuilder):
     java_executable_path: Path
     args: list[str]
     world_path: str | None # Necessary because jmc2OBJ expects the world path last
+    xz_min_max: tuple[int, int, int, int] | None # Used for offsets
 
     @override
     def set_world_path(self, world: Path) -> None | MCprepError:
@@ -25,20 +26,38 @@ class Jmc2OBJExportBuilder(ExportBuilder):
         base_check = super().set_export_bounds(min_bounds, max_bounds)
         if base_check is not None:
             return base_check
-        self.args += [f"--area={min_bounds[0]},{min_bounds[2]},{max_bounds[0]},{max_bounds[2]}",
+
+        self.xz_min_max = (min_bounds[0],min_bounds[2],max_bounds[0],max_bounds[2])
+        self.args += [f"--area={','.join(str(x) for x in self.xz_min_max)}",
                       f"--height={min_bounds[1],max_bounds[1]}"]
 
-    # TODO: Ask the jmc2OBJ devs for a way to set the Y-offset,
-    # or otherwise figure out a way to determine if the original
-    # OBJ was centered or used an arbitrary offset
     @override
     def set_export_offset(self, offset: tuple[float, float, float]) -> None | MCprepError:
-        # Since there's no way to have arbitrary Y offsets
-        # with jmc2OBJ, it's better to alert the end-user
+        # If a Y-offset other than 0 is used,
+        # assume it was centered. Otherwise, check
+        # if the X and Z are centered (to handle the
+        # edge case of an export with minY set to 0)
+        # Source: https://github.com/jmc2obj/j-mc-2-obj/blob/master/src/org/jmc/ObjExporter.java#L98-L103
         if offset[1] != 0:
-            line, file = env.current_line_and_file()
-            return MCprepError(NotImplementedError(), line, file, "jmc2OBJ cannot take an arbitrary Y offset!")
-        self.args.append(f"--offset={offset[0]},{offset[2]}")
+            self.args.append("--offset=center")
+            return
+
+        # Kind of a horrifying block, but this
+        # is enough to make Mypy not freak out from
+        # type narrowing
+        def average_offset(m: int, n: int) -> int:
+            return int(-1 * (m + (n - m) / 2))
+
+        # Use an assert as this function should
+        # be called after the bounds are set
+        assert self.xz_min_max is not None
+
+        x_average = average_offset(self.xz_min_max[0], self.xz_min_max[2])
+        z_average = average_offset(self.xz_min_max[1], self.xz_min_max[3])
+        if offset[0] == x_average and offset[1] == z_average:
+            self.args.append("--offset=center")
+        else:
+            self.args.append(f"--offset={offset[0]},{offset[2]}")
 
     @override
     def set_block_scale(self, scale: float) -> None | MCprepError:
